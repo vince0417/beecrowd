358e64490359772ce934cf3c92cefd2e99c96e3f
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 23:06:37 -0300

    Média 1

---
+++ c/iniciante/1005.c
@@ -0,0 +1,11 @@
+//Média 1
+#include <stdio.h>
+
+int main()
+{
+    double a, b, media;
+    scanf("%lf %lf", &a, &b);
+    media = (a * 3.5 + b * 7.5) / 11.0;
+    printf("MEDIA = %.5lf\n", media);
+    return 0;
+}
