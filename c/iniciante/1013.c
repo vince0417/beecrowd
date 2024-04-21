61f3f9fa5bd728ed9670dff7be8161ed05429d93
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 23:15:26 -0300

    O Maior

---
+++ c/iniciante/1013.c
@@ -0,0 +1,13 @@
+//O Maior
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c, maior;
+    scanf("%d %d %d", &a, &b, &c);
+    maior = (a + b + abs(a - b)) / 2;
+    maior = (maior + c + abs(maior - c)) / 2;
+    printf("%d eh o maior\n", maior);
+    return 0;
+}
