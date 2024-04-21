41d199ecfd6e2952196c24c4f9cca868708186f6
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:38:15 -0300

    Área Direita

---
+++ c++/iniciante/1190.cpp
@@ -0,0 +1,36 @@
+//Área Direita
+#include <iostream>
+using namespace std;
+
+int main() {
+    double matriz[12][12], soma = 0.0;
+    char op;
+    cin >> op;
+
+    for (int l = 0; l < 12; l++)
+    {
+        for (int c = 0; c < 12; c++)
+        {
+            cin >> matriz[l][c];
+
+            if (l < c and l+c > (12-1))
+            {
+                soma += matriz[l][c];
+            }
+            
+        }
+        
+    }
+    
+    cout.precision(1);
+    cout.setf(ios::fixed);
+    if (op == 'S')
+    {
+        cout << soma << endl;
+    }
+    else{
+        cout << soma/(((12*12)-24)/4) << endl;
+    }
+    
+    return 0;
+}
