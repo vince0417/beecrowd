e2bd8d27306f83737e08f0aa5b2fc0de1c9037c6
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:45:39 -0300

    Senha

---
+++ c++/iniciante/2146.cpp
@@ -0,0 +1,14 @@
+//Senha
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int senha;
+
+    while (cin >> senha)
+    {
+        cout << senha-1 << endl;
+    }
+    
+    return 0;
+}
