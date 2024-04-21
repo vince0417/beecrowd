7c7820635abbc85276b125fd05816c05504236f6
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:43:11 -0300

    Vitória e a Indecisão

---
+++ c++/iniciante/1924.cpp
@@ -0,0 +1,17 @@
+//Vitória e a Indecisão
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num+1; i++)
+    {
+        string curso;
+        getline(cin, curso);
+    }
+
+    cout << "Ciencia da Computacao" << endl;
+    return 0;
+}
