729ee5d3219ce4d5b3e0c77f9038b716859330d6
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:39:42 -0300

    Bob Conduite

---
+++ c++/iniciante/1589.cpp
@@ -0,0 +1,18 @@
+//Bob Conduite
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+
+        cout << n1+n2 << "\n";
+    }
+    
+    return 0;
+}
