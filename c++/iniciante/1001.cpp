9a8d74843c3751883f9646aef4385cfbbb68a14d
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 21:19:14 -0300

    Extremamente Básico

---
+++ c++/iniciante/1001.cpp
@@ -0,0 +1,12 @@
+//Extremamente Básico
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, x = 0;
+    cin >> a >> b ;
+
+    x += a + b;
+    cout << "X = " << x << endl;
+    return 0;
+}
