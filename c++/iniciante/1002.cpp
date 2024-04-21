ed7699f3252605754034cfb9bc2d9959e09e0c8e
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 21:19:59 -0300

    Área do Círculo

---
+++ c++/iniciante/1002.cpp
@@ -0,0 +1,14 @@
+//Área do Círculo
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double const pi = 3.14159;
+    double raio;
+    cin >> raio;
+    cout.precision(4);
+    cout.setf(ios::fixed);
+    cout << "A=" << pi*raio*raio << endl;
+
+    return 0;
+}
