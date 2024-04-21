7e95ef3bac84242f45b6aaeedbfde6db65b81dd8
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:43:48 -0300

    Tomadas

---
+++ c++/iniciante/1930.cpp
@@ -0,0 +1,13 @@
+//Tomadas
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t1, t2, t3, t4;
+    cin >> t1 >> t2 >> t3 >> t4;
+
+    int total = (t1+t2) + (t3+t4) - 3;
+    cout << total << endl;
+
+    return 0;
+}
