a27c4dff1976e4b73c37b64c87c5e8b1ff93097b
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 23:19:22 -0300

    Distância

---
+++ c/iniciante/1016.c
@@ -0,0 +1,12 @@
+//Distância
+#include <stdio.h>
+
+int main()
+{
+    int minutos;
+    int distancia;
+    scanf("%d", &distancia);
+    minutos = distancia + distancia;
+    printf("%d minutos\n", minutos);
+    return 0;
+}
