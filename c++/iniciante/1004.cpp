d58dca2987a25f0b749ea236717879af0dcdc993
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 21:22:11 -0300

    Produto Simples

---
+++ c++/iniciante/1004.cpp
@@ -0,0 +1,11 @@
+//Produto Simples
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int PROD, num;
+    cin >> PROD >> num;
+    cout << "PROD = "<< PROD*num << endl;
+
+    return 0;
+}
