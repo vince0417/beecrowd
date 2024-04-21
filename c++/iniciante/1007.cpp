da0986968f77af09645d62509d8ed0dd5db44a49
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 21:24:41 -0300

    Diferença

---
+++ c++/iniciante/1007.cpp
@@ -0,0 +1,13 @@
+//Diferença
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2, n3, n4;
+    cin >> n1 >> n2 >> n3 >> n4;
+
+    int diferenca = (n1*n2)-(n3*n4);
+    cout << "DIFERENCA = " << diferenca << endl;
+
+    return 0;
+}
