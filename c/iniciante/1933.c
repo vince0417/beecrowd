66297df0060ab8968b9f44a08698513e0f141422
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 23:43:58 -0300

    Tri-du

---
+++ c/iniciante/1933.c
@@ -0,0 +1,16 @@
+//Tri-du
+#include <stdio.h>
+
+int main() {
+    int c1, c2;
+    scanf("%d %d", &c1, &c2);
+
+    if (c1 > c2)
+    {
+        printf("%d\n", c1);
+    }
+    else
+        printf("%d\n", c2);
+
+    return 0;
+}
