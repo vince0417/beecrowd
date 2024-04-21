274440d77f4de7e3122656521034684803a5f52d
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:38:56 -0300

    Vai Ter Copa?

---
+++ c++/iniciante/1564.cpp
@@ -0,0 +1,19 @@
+//Vai Ter Copa?
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    while (cin >> num)
+    {
+        if (num > 0)
+        {
+            cout << "vai ter duas!" << endl;
+        }
+        else 
+            cout << "vai ter copa!" << endl;
+             
+    }
+    
+    return 0;
+}
