5b205de6bb30bc68c837fe25f3cd409e15699bd6
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 23:31:09 -0300

    Números Pares

---
+++ c/iniciante/1059.c
@@ -0,0 +1,15 @@
+//Números Pares
+#include <stdio.h>
+
+int main () {
+    int i;
+
+    for(i = 2; i <= 100; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d\n", i);
+        }       
+    }
+    return 0;
+}
