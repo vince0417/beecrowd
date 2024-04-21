66f8e4e4a573f84d3312ba44f3acd91610084e2b
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:50:27 -0300

    Volta à Faculdade de Física

---
+++ c++/matematica/1197.cpp
@@ -0,0 +1,15 @@
+//Volta à Faculdade de Física
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v, t;
+
+    while (cin >> v >> t)
+    {
+        int resultado = v*2*t;
+        cout << resultado << endl;
+    }
+    
+    return 0;
+}
