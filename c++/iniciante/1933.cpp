97693bf5bf599efea899810529bd1d7b12852388
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-20 22:44:24 -0300

    Tri-du

---
+++ c++/iniciante/1933.cpp
@@ -0,0 +1,17 @@
+//Tri-du
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int c1, c2;
+    cin >> c1 >> c2;
+
+    if (c1 > c2)
+    {
+        cout << c1 << endl;
+    }
+    else
+        cout << c2 << endl;
+
+    return 0;
+}
