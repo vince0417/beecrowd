3bfa247b39ea08ec6f54eb402cf772b691ff4462
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-22 00:27:05 -0300

    Corrida

---
+++ c++/ad-hoc/2416.cpp
@@ -0,0 +1,11 @@
+//Corrida
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long metros, pista;
+    cin >> metros >> pista;
+    cout << metros%pista << endl;
+    return 0;
+}
