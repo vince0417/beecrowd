9fd477f81fad301fa69feadbe52896277310e408
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-24 12:32:58 -0300

    Og

---
+++ c++/ad-hoc/1387.cpp
@@ -0,0 +1,16 @@
+//Og
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int m1, m2;
+
+    while (cin >> m1 >> m2)
+    {
+        if (m1 == 0 and m2 == 0) break;
+        cout << m1+m2 << endl;
+    }
+    
+    return 0;
+}
