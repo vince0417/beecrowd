7621c145f826dd8eb78e4b8b7189bc7caaba4af5
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-25 10:22:12 -0300

    A Saída

---
+++ c++/iniciante/2861.cpp
@@ -0,0 +1,18 @@
+//A Saída
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num+1; i++)
+    {
+        string pergunta;
+        getline(cin, pergunta);
+    }
+    for (int i = 0; i < num; i++) cout << "gzuz\n";
+
+    return 0;
+}
