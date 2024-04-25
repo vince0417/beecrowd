a81a0f51e702d651453ca1a9d866ab3900bc4041
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-24 21:09:45 -0300

    Evento

---
+++ c++/iniciante/2172.cpp
@@ -0,0 +1,15 @@
+//Evento
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long n1, n2;
+    while (cin >> n1 >> n2)
+    {
+        if (n1 == 0 and n2 == 0) break;
+        cout << n1*n2 << endl;
+    }
+    
+    return 0;
+}
