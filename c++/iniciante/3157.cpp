0f449804a80fb875877c9d16d92050eb5b89ce7c
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-28 17:32:37 -0300

    Viva a Ciência!

---
+++ c++/iniciante/3157.cpp
@@ -0,0 +1,11 @@
+//Viva a Ciência!
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string frase;
+    getline(cin, frase);
+    cout << "Nao se refuta ciencia com opiniao\n";
+    return 0;
+}
