e56dc990facedcdae8a649592baa2afd43012a1e
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-28 21:00:18 -0300

    Charadas no Escuro

---
+++ c++/iniciante/3146.cpp
@@ -0,0 +1,16 @@
+//Charadas no Escuro
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    const float pi = 3.14;
+    float num;
+    cin >> num;
+
+    cout.precision(2);
+    cout.setf(ios::fixed);
+    cout << 2*pi*num << endl;
+    return 0;
+}
