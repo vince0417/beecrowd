585b185f9aa0623547ebbbdae4327970c1b623f3
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-04-29 23:43:00 -0300

    Palavrão

---
+++ c++/iniciante/3303.cpp
@@ -0,0 +1,13 @@
+//Palavrão
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string palavra;
+    cin >> palavra;
+
+    if (palavra.size() >= 10) cout << "palavrao\n";
+    else cout << "palavrinha\n";
+    return 0;
+}
