5a6cc5582aefbf5eb69d93d1d97ca4ff0a775937
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-05-03 22:26:35 -0300

    Resposta Certa

---
+++ c++/iniciante/3302.cpp
@@ -0,0 +1,18 @@
+//Resposta Certa
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        int x;
+        cin >> x;
+        cout << "resposta " << i << ": " << x << endl;
+    }
+
+    return 0;
+}
