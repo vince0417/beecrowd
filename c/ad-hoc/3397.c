45dcb83dbe41484512c98a684d318fdf98477cf4
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-05-20 22:54:12 -0300

    Melhor do Multiverso

---
+++ c/ad-hoc/3397.c
@@ -0,0 +1,14 @@
+//Melhor do Multiverso
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char frase[100];
+    while (fgets(frase, 99, stdin))
+    {
+        printf("eu\n");
+    }
+    
+    return 0;
+}
