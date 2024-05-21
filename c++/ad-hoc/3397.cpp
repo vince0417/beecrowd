482b2b20317f131b2e6f6beaf2d9471a63d7713d
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-05-20 22:34:01 -0300

    Melhor do Multiverso

---
+++ c++/ad-hoc/3397.cpp
@@ -0,0 +1,14 @@
+//Melhor do Multiverso
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string frase;
+    while (getline(cin, frase))
+    {
+        cout << "eu\n";
+    }
+    
+    return 0;
+}
