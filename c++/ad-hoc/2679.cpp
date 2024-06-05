001e88ecca94d4cd57947098d4a73db8b21676a1
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-04 22:04:45 -0300

    Sucessor Par

---
+++ c++/ad-hoc/2679.cpp
@@ -0,0 +1,14 @@
+//Sucessor Par
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if ((num+1)%2 == 0) cout << (num+1) << endl;
+    else cout << (num+2) << endl;
+
+    return 0;
+}
