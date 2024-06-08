2f0f3e81ceb96870e79aafcd4c32cc7729967428
Vicente Rodrigues <vicenterodrigues564382gmail.com>
2024-06-08 13:12:18 -0300

    Crescente e Decrescente

---
+++ c/iniciante/1113.c
@@ -0,0 +1,23 @@
+//Crescente e Decrescente
+#include <stdio.h>
+
+int main () {
+    int x, y;
+
+    do
+    {
+        scanf("%d%d", &x, &y);
+
+        if (x>y)
+        {
+            printf("Decrescente\n");
+        }
+        else if (x<y)
+        {
+            printf("Crescente\n");
+        }
+
+    } while (x!=y);
+
+    return 0;
+}
