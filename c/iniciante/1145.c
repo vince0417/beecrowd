a4e691ef54b9baedbded8f347982b78905083789
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-09 08:08:00 -0300

    Sequência Lógica 2

---
+++ c/iniciante/1145.c
@@ -0,0 +1,34 @@
+//Sequência Lógica 2
+#include <stdio.h>
+
+int main() {
+    int n1, n2, cont = 0;
+    scanf("%i%i", &n1, &n2);
+
+    for (int i = 1; i <= n1; i++)
+    {
+        cont += 1;
+        if (cont < n1) printf("%i ", i);
+
+        if (cont == n1)
+        {
+            printf("%i\n", i);
+            cont = 0;
+        }
+        
+    }
+
+    for (int i = n1+1; i <= n2; i++)
+    {
+         cont += 1;
+        if (cont < n1) printf("%i ", i);
+
+        if (cont == n1)
+        {
+            printf("%i\n", i);
+            cont = 0;
+        }
+    }
+     
+    return 0;
+}
