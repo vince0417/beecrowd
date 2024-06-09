2b037b1298dda8e86eb25c50d190ed782a71ba14
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-09 09:27:18 -0300

    Soma de Pares Consecutivos

---
+++ c/iniciante/1159.c
@@ -0,0 +1,28 @@
+//Soma de Pares Consecutivos
+#include <stdio.h>
+
+int main() {
+    int x, cont = 0, soma = 0;
+    
+    while (1)
+    {
+        scanf("%i", &x);
+        if (x == 0) break;
+        
+        while (cont < 5)
+        {
+            if (x%2 == 0)
+            {
+                soma += x;
+                cont += 1;
+            }
+            x++;
+        }
+        
+        printf("%i\n", soma);
+        soma = 0;
+        cont = 0;
+    }
+   
+    return 0;
+}
