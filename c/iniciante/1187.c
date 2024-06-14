40163c0ec1e716c4e0eaf191228bf937539e86b4
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-13 21:24:38 -0300

    Área Superior

---
+++ c/iniciante/1187.c
@@ -0,0 +1,28 @@
+//Área Superior
+#include <stdio.h>
+
+int main()
+{
+    double matriz[12][12], soma = 0.0;
+    char op;
+    scanf("%c", &op);
+
+    for (int l = 0; l < 12; l++)
+    {
+        for (int c = 0; c < 12; c++)
+        {
+            scanf("%lf", &matriz[l][c]);
+            if (c > l && l+c < (12-1))
+            {
+                soma += matriz[l][c];
+            }
+            
+        }
+        
+    }
+
+    if (op == 'S') printf("%.1lf\n", soma);
+    else printf("%.1lf\n", soma/(((12*12)-24)/4));
+
+    return 0;
+}
