62c2345a72f40ceefd7aaef848445439fc66b8d9
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-30 18:40:06 -0300

    Somando Inteiros Consecutivos

--- c++/iniciante/1149.cpp
+++ c++/iniciante/1149.cpp
@@ -13,12 +13,7 @@ int main()
         if (n2 <= 0) while (n2 <= 0) cin >> n2;
     }
 
-    int soma {0};
-    for (int i {0}; i < n2; i++)
-    {
-        soma += (n1+i);
-    }
-    
+    int soma = (n2*n1)+((n2*(n2-1))/2);//Utilizado PA
     cout << soma << endl;
     return 0;
 }
