8134ff2c25e8591142b732ad59ff2e4210636331
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-07-02 20:14:32 -0300

    Boa divisão

---
+++ c/ad-hoc/3474.c
@@ -0,0 +1,14 @@
+//Boa divisão
+#include <stdio.h>
+
+int main()
+{
+    float n1;
+    int n2;
+    scanf("%f %i", &n1, &n2);
+
+    float divisao = (n1/n2);
+    
+    printf("%.2f\n", divisao);
+    return 0;
+}
