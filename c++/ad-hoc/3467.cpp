b2cf95a7a3817e760548fc69f559c46271ff9118
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-07-06 10:55:02 -0300

    Esse é o meu lugar!

---
+++ c++/ad-hoc/3467.cpp
@@ -0,0 +1,16 @@
+//Esse é o meu lugar!
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string sofa;
+
+    while (cin >> sofa)
+    {
+        if (sofa == "xxL") cout << "Esse eh o meu lugar\n";
+        else cout << "Oi, Leonard\n";
+    }
+    
+    return 0;
+}
