783349439985694a931cc1b5941d3cde43dcfbdc
vince0417 <vicenterodrigues56348@gmail.com>
2025-01-08 21:11:38 -0300

    Sequência Secreta

---
+++ c++/ad-hoc/3048.cpp
@@ -0,0 +1,24 @@
+//Sequência Secreta
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int total {}, aux {}, num {2}, resposta {};
+    cin >> total;
+    
+    for (int i = 0; i < total; i++)
+    {
+        cin >> aux;
+
+        if (aux != num) 
+        {
+            resposta++;
+            num = aux;
+        }
+    
+    }
+
+    cout << resposta << '\n';
+    return 0;
+}
