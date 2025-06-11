9606bde2972698eabb5c8e1af7c3951be73cedc3
vince0417 <vicenterodrigues56438@gmail.com>
2025-06-11 00:01:01 -0300

    Copiando e Colando Código

---
+++ c++/iniciante/3140.cpp
@@ -0,0 +1,24 @@
+//Copiando e Colando Código
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string linha{}, body{};
+
+    int inicioBody{}, fimBody{}, cont{};
+    do 
+    {
+        getline(cin, linha);
+        if (linha == "    </body>") fimBody = cont;
+        if (inicioBody > 0 and fimBody == 0)
+        {
+            body += linha + "\n";
+        } 
+        if (linha == "    <body>") inicioBody = cont;
+        cont++;
+    } while (linha != "</html>");
+    
+    cout << body;
+    return 0;
+}
