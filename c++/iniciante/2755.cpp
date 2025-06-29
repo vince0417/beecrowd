5887466e5c545647f5dc81bcc2f1e59199ac494f
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-06-29 18:08:38 -0300

    Saída 9

---
+++ c++/iniciante/2755.cpp
@@ -0,0 +1,12 @@
+//Saída 9
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "\"Ro'b'er\tto\\/\"" << "\n";
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << "\n";
+    cout << "(^_-) (-_-) (-_^)" << "\n";
+    cout << "(\"_\") ('.')" << "\n";
+	return 0;
+}
