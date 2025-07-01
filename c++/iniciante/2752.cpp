1cf2873a9689737bfd084c9a87747c5cbbd35ca3
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-06-30 22:06:31 -0300

    Saída 6

---
+++ c++/iniciante/2752.cpp
@@ -0,0 +1,17 @@
+//Saida 6
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "<AMO FAZER EXERCICIO NO URI>\n";
+    cout << "<    AMO FAZER EXERCICIO NO URI>\n";
+    cout << "<AMO FAZER EXERCICIO >\n";
+    cout << "<AMO FAZER EXERCICIO NO URI>\n";
+    cout << "<AMO FAZER EXERCICIO NO URI    >\n";
+    cout << "<AMO FAZER EXERCICIO NO URI>\n";
+    cout << "<          AMO FAZER EXERCICIO >\n";
+    cout << "<AMO FAZER EXERCICIO           >\n";
+    
+    return 0;
+}
