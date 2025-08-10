865607f42201b30937eba601616b606884f1b292
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-08-09 21:48:03 -0300

    Polígonos Regulares Simples

---
+++ c/iniciante/1959.c
@@ -0,0 +1,11 @@
+//Polígonos Regulares Simples
+#include <stdio.h>
+
+int main()
+{
+    long long lados = 0, comprimento = 0;
+    scanf("%lld %lld", &lados, &comprimento);
+
+    printf("%lld\n", (lados*comprimento));
+    return 0;
+}
