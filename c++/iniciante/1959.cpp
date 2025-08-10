53a66117daa493e646f228447e3f28108902e8d0
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-08-09 21:41:26 -0300

    Polígonos Regulares Simples

---
+++ c++/iniciante/1959.cpp
@@ -0,0 +1,11 @@
+//Polígonos Regulares Simples
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long lados{}, comprimento{};
+    cin >> lados >> comprimento;
+    cout << (lados*comprimento) << "\n";
+    return 0;
+}
