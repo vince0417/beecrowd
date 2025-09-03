f325c295d1dc6b1b30d44f1c034a5fa4ecf3febf
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-09-03 00:24:11 -0300

    Entrada e Saída Lendo e Pulando Nomes

---
+++ c/iniciante/2762.c
@@ -0,0 +1,14 @@
+//Entrada e Saída Lendo e Pulando Nomes
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome[30];
+    for (int i = 1; i <= 10; i++)
+    {
+        scanf("%s", nome);
+        if (i == 3 || i == 7 || i == 9) printf("%s\n", nome);
+    }
+    return 0;
+}
