d611326cfc193db0244982a58dd9c6f74a6b5798
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-09-18 19:08:41 -0300

    O Enigma do Pronalândia

---
+++ c++/iniciante/1984.cpp
@@ -0,0 +1,11 @@
+//O Enigma do Pronalândia
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string num;
+    cin >> num;
+    reverse(num.begin(), num.end());
+    cout << num << "\n";
+    return 0;
+}
