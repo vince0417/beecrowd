52e84471e742efcfe874c18e2fc74de951f433dd
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-09-27 21:33:46 -0300

    Guilherme e Suas Pipas

---
+++ c++/matematica/1921.cpp
@@ -0,0 +1,11 @@
+//Guilherme e Suas Pipas
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long lados;
+    cin >> lados;
+    long barbantes = ((double)lados/2)*(lados-3);
+    cout << barbantes << "\n";
+    return 0;
+}
