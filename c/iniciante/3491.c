a40d4bd18e5b712545a20ef6adf80fa5129717a3
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2026-04-17 16:57:30 -0300

    Bilhete codificado

--- c/iniciante/3491.c
+++ c/iniciante/3491.c
@@ -4,7 +4,14 @@
 int main(){
     int num = 0;
     scanf("%i", &num);
-    char letra = '`'+num;
+    char letra = '`'+num; // Suponha que num é 10, o caractere '`' + 10 é 'j' na tabela ASCII;
     printf("%c\n",letra);
     return 0;
 }
+
+/*
+    Usei Aritmética de caracteres para resolver este problema.
+    Como num será a posição de determinada letra do alfabeto e, sabemos que a letra é minúscula,
+    fazendo uso da tabela ASCII podemos descobrir a letra sem criar um array contendo as letras do alfabeto.
+    'a' tem como valor ASCII 97, por isso pegamos o caractere '`', que antecede 'a', como base. 
+*/
